cbf181d471ffd551e5d8f591616d15593f3c5260
Fariz Luqman <farizluqman@simplelinux.my>
2014-04-23 22:08:30 +0800

    Update module-edit.vb

--- module-edit.vb
+++ module-edit.vb
@@ -1,4 +1,4 @@
-﻿Imports System.Data.OleDb
+Imports System.Data.OleDb
 
 Module module_edit
     Public Function editRecord(ByVal meselectMenu As String, ByVal menu As String, ByVal price As String, ByVal quantity As String) As Boolean
@@ -8,7 +8,7 @@ Module module_edit
             Dim connection As New OleDbConnection("Provider=Microsoft.Jet.OLEDB.4.0;Data Source=Database.mdb;")
             connection.Open()
             ' Declare what command to be executed when the function started
-            Dim command As New OleDbCommand("UPDATE tblMenu SET Menu=inmenu, Price=imprice, Quantity=imquantity WHERE Menu = '" & meselectMenu & "'", connection)
+            Dim command As New OleDbCommand("UPDATE tblMenu SET Menu=inmenu, Price=imprice, Quantity=imquantity WHERE Menu = inmenu", connection)
             command.Parameters.AddWithValue("inmenu", menu)
             command.Parameters.AddWithValue("imprice", price)
             command.Parameters.AddWithValue("imquantity", quantity)
