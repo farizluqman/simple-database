79222f97b867a6139306e7ae3da68c54a4c12460
Fariz Luqman <farizluqman@simplelinux.my>
2014-04-24 03:21:37 +0800

    Update module-delete.vb

--- module-delete.vb
+++ module-delete.vb
@@ -9,7 +9,7 @@ Module module_delete
             Dim connection As New OleDbConnection("Provider=Microsoft.Jet.OLEDB.4.0;Data Source=Database.mdb;")
             connection.Open()
             ' Declare what command to be executed when the function started
-            Dim command As New OleDbCommand("Delete * FROM tblMenu where Menu = inmenu", connection)
+            Dim command As New OleDbCommand("Delete * FROM [tblMenu] where [Menu] = inmenu", connection)
             command.Parameters.AddWithValue("inmenu", meselectMenu)
             command.ExecuteNonQuery()
             connection.Close()
