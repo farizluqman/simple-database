a1cf82d51900e2acf4745dbebbf720d2c4c72ae8
Fariz Luqman <farizluqman@simplelinux.my>
2014-04-23 22:06:52 +0800

    Updated the module-editrecord
    - using the parameter function to avoid conflicts
    - safe from database injection

--- module-addrecord.vb
+++ module-addrecord.vb
@@ -1,4 +1,4 @@
-﻿Imports System.Data.OleDb
+Imports System.Data.OleDb
 
 Module module_addrecord
     Public Function addRecord(ByVal menu As String, ByVal price As String, ByVal quantity As String) As Boolean
@@ -8,8 +8,10 @@ Module module_addrecord
             Dim connection As New OleDbConnection("Provider=Microsoft.Jet.OLEDB.4.0;Data Source=Database.mdb;")
             connection.Open()
             ' Declare what command to be executed when the function started
-            Dim command As New OleDbCommand("INSERT INTO tblMenu (Menu, Price, Quantity) VALUES ('" & menu & "', '" & price & "', '" & quantity & "')", connection)
-
+            Dim command As New OleDbCommand("INSERT INTO tblMenu (Menu, Price, Quantity) VALUES (inmenu, inprice, inquantity)", connection)
+            command.Parameters.AddWithValue("inmenu", menu)
+            command.Parameters.AddWithValue("inprice", price)
+            command.Parameters.AddWithValue("inquantity", quantity)
             ' start the connection
             command.ExecuteNonQuery()
             connection.Close()
