c213e8441d8d626619a742f22ada126a7cd34667
Fariz Luqman <farizluqman@simplelinux.my>
2014-04-23 22:09:10 +0800

    Updated the module-delete
    - using the parameter function to avoid conflicts
    - safe from database injection

--- module-delete.vb
+++ module-delete.vb
@@ -1,4 +1,4 @@
-﻿Imports System.Data.OleDb
+Imports System.Data.OleDb
 
 Module module_delete
     Public Function deleteRecord(ByVal meselectMenu As String) As Boolean
@@ -9,7 +9,8 @@ Module module_delete
             Dim connection As New OleDbConnection("Provider=Microsoft.Jet.OLEDB.4.0;Data Source=Database.mdb;")
             connection.Open()
             ' Declare what command to be executed when the function started
-            Dim command As New OleDbCommand("Delete * FROM tblMenu where Menu = '" & meselectMenu & "'", connection)
+            Dim command As New OleDbCommand("Delete * FROM tblMenu where Menu = inmenu", connection)
+            command.Parameters.AddWithValue("inmenu", meselectMenu)
             command.ExecuteNonQuery()
             connection.Close()
             connection.Dispose()
